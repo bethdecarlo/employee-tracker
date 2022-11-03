ef84c11371abfa9096aa0832d5e1d226ce55addb
bethdecarlo <bethdecarlo@gmail.com>
2022-11-02 22:29:08 -0400

    added inquierer prompt to app.js

--- app.js
+++ app.js
@@ -23,3 +23,22 @@ connection.connect(err => {
     console.log('successfully connected to employee_tracker_db');
     start();
 });
+
+// begins inquierer prompt
+function start() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a new department",
+            "Add a new role",
+            "Add a new employee",
+            "Update an employee's role",
+            "Exit app"
+        ]}
+    )
+    // loops through prompt choices
