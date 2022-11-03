f995b25b0341fb8908928f463508c0f947292af1
bethdecarlo <bethdecarlo@gmail.com>
2022-11-02 22:19:26 -0400

    added function to connect to database on app.js

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ figlet("Welcome to \n \n Employee \n \n Tracker!", (err, data) => {
     if (err) throw err;
     console.log(data);
 })
+
+//function to connect to database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3000,
+    user: "root",
+    password: "",
+    database: "employee_tracker_db"
+});
