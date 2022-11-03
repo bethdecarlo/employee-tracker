e89094115f7a8386468d07993a11d135895e1adc
bethdecarlo <bethdecarlo@gmail.com>
2022-11-02 22:14:14 -0400

    added figlet title to app.js

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 const figlet = require("figlet");
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+//title of app... powered by figlet!
+figlet("Welcome to \n \n Employee \n \n Tracker!", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+})
