d103b349ddb8188f7c3dacee122f2500f6d1536e
bethdecarlo <bethdecarlo@gmail.com>
2022-11-02 23:00:58 -0400

    updated syntax for addNewRole function on app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ figlet("Welcome to \n \n Employee \n \n Tracker!", (err, data) => {
 //function to connect to database
 const connection = mysql.createConnection({
     host: "localhost",
-    port: 3000,
+    port: 3306,
     user: "root",
     password: "rootroot",
     database: "employee_tracker_db"
@@ -46,27 +46,27 @@ function start() {
         then((answer) => {
             switch (answer.action) {
                 case "View all departments":
-                    viewDepts();
+                    viewAllDepts();
                     break;
     
                 case "View all roles":
-                    viewRoles();
+                    viewAllRoles();
                     break;
     
                 case "View all employees":
-                    viewEes();
+                    viewAllEmployees();
                     break;
     
                 case "Add a department":
-                    addDept();
+                    addNewDept();
                     break;
     
                 case "Add a role":
-                    addRole();
+                    addNewRole();
                     break;
     
                 case "Add an employee":
-                    addEe();
+                    addNewEmployee();
                     break;
     
                 case "Update employee role":
@@ -137,5 +137,74 @@ function addNewDept() {
                 start();
             }
         );
+    });
+}
+
+//function that will add new role 
+// loops through prompting new role, role's salary, and role's department
+
+function addNewRole() {
+    const sql = "SELECT * FROM department";
+    connection.query(sql, (err, results) => {
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What is the name of the new role?",
+                validate: (value) => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Try again. New role not added.");
+                    }
+                }
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is this role's salary?",
+                validate: (value) => {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    console.log("Try again. Please enter a number value for the salary.");
+                }
+            },
+            {
+                name: "department",
+                type: "rawlist",
+                choices: () => {
+                    let choiceArray = [];
+                    for (let i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].name);
+                    }
+                    return choiceArray;
+                },
+                message: "What department will this new role be in?",
+            }
+        ]).then(answer => {
+            let chosenDept;
+            for (let i = 0; i < results.length; i++) {
+                if (results[i].name === answer.department) {
+                    chosenDept = results[i];
+                }
+            }
+
+            connection.query(
+                "INSERT INTO role SET ?",
+                {
+                    title: answer.title,
+                    salary: answer.salary,
+                    department_id: chosenDept.id
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log(`New role ${answer.title} has been added!`);
+                    start();
+                }
+            )
+        });
     });
 }
